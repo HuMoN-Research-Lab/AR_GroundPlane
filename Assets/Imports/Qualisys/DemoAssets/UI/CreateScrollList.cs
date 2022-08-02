43a382fc003279cdb33753bd3fdb5957558383fd
trentwirth <62706609+trentwirth@users.noreply.github.com>
2022-08-02 09:52:18 -0400

    trentwirth_dev1 (#13)
    * recreated folder structure
    
    Recreated folder structure from the old version of the ARGP, complete with having copied over all of the contents of each of the folders.
    
    Everything folder and file wise should now exist in the folder to run the experiment, except for the qualisys folders. I'll include that in next commit
    
    * added qualisys SDK to project
    
    they claim they've only tested it up to Unity 2019.4.23f1.
    
    this could pose a problem later... keeping an eye on things.
    
    * added unity objects
    
    added all objects to scene except for UXF_Rig and EventSystem
    
    * connected scripts to game objects
    
    began the process of reconnecting scripts to game objects. Ran into an issue (that I've had before) where the GenerateTerrain method of the class LoadTerrain didn't show up in the UXF rig. Need to investigate this problem further.

---
+++ Assets/Imports/Qualisys/DemoAssets/UI/CreateScrollList.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using QualisysRealTime.Unity;
+using QTMRealTimeSDK;
+
+public class CreateScrollList : MonoBehaviour {
+
+    public GameObject serverButtonPrefab; 
+    public Transform contentPanel;
+    private List<DiscoveryResponse> discoveryResponses;
+
+    void Update()
+    {
+        if (RTClient.GetInstance().GetStreamingStatus())
+        {
+            Disable();
+        }
+
+        if (contentPanel.childCount < 1)
+        {
+            UpdateList();
+        }
+    }
+    public void UpdateList()
+    {
+        for (int i = 0; i < contentPanel.childCount; i++)
+        {
+            Destroy(contentPanel.GetChild(i).gameObject);
+        }
+        discoveryResponses = RTClient.GetInstance().GetServers();
+        foreach (DiscoveryResponse server in discoveryResponses)
+        {
+            GameObject newServer = Instantiate(serverButtonPrefab) as GameObject;
+            ServerButton serverButton = newServer.GetComponent<ServerButton>();
+            serverButton.HostText.text = server.HostName;
+            serverButton.IpAddressText.text = server.IpAddress + ":" + server.Port;
+            serverButton.InfoText.text = server.InfoText;
+            serverButton.response = server;
+            newServer.transform.SetParent(contentPanel);
+        }
+    }
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
