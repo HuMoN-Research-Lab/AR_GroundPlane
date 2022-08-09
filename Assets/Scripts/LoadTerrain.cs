918a6a5dc506194e11cd84625582743fa249623e
trentwirth <62706609+trentwirth@users.noreply.github.com>
2022-08-09 17:16:31 -0400

    Trentwirth/in lab dev scripts and game objects (#19)
    * connected scripts to game objects
    
    added scripts to game objects that interact with the UXF Event System. Including: start session, start trial, end trial. Connected LoadTerrain.GenerateTerrain to the UXF Rig via the gameObject `ScriptHolder`.
    
    Removed the distractor, and changed the target to a bullseye. Connected audio trigger script to the bullseye target, and set the dwell-time-threshold to 20 (i.e., what it was set at before, established via testing with Michael)
    
    * targets & bad bar-logic
    
    targets are being built, hooray! But, the start and end bar logic is not working - bad.
    
    Goal for near future: attach scripts to game-objects with an "Initialization" script, so I can get away from drag and drop.
    
    * cleaned up code w/ Jon
    
    * refactored LoadTerrain.cs

--- Assets/Scripts/LoadTerrain.cs
+++ Assets/Scripts/LoadTerrain.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System;
+using System;
 using UnityEngine;
 
 using UXF;
@@ -11,262 +11,75 @@ public class LoadTerrain : MonoBehaviour
     //public Session session;
     public GameObject prefab_distractor;
     public GameObject prefab_target;
-    public GameObject Timer_Start_Box_Left;
-    public GameObject Timer_Start_Box_Right;
-    public GameObject Trial_End_Box_Left;
-    public GameObject Trial_End_Box_Right;
+    public GameObject Start_Bar_Left;
+    public GameObject Start_Bar_Right;
+    public GameObject End_Bar_Left;
+    public GameObject End_Bar_Right;
 
     public bool is_mirrored;
+
+    private string file_name;
+    private TextAsset terrain_file;
 
     public void GenerateTerrain(Trial trial)
     {
 
         Debug.Log("Terrain is Loaded");
-        //Debug.Log(is_mirrored);
+        Debug.Log("is_mirrored:" + is_mirrored);
         // get the positions of each of the projectors
         var Projector1_Pos_XYZ = GameObject.Find("CamProjector1").transform.position;
         var Projector2_Pos_XYZ = GameObject.Find("CamProjector2").transform.position;
         var Projector3_Pos_XYZ = GameObject.Find("CamProjector3").transform.position;
 
         // Get file name from the world
-        if(is_mirrored == false)
+        if(is_mirrored)
         {
-            Timer_Start_Box_Left.SetActive(true);
-            Timer_Start_Box_Right.SetActive(false);
-
-            Trial_End_Box_Left.SetActive(false);
-            Trial_End_Box_Right.SetActive(true);
-            
-            string file_name = trial.settings.GetString("File");
+            file_name = trial.settings.GetString("Mirror");
+            Debug.Log("is_mirrored = True");
+        }
+        else
+        {
+            file_name = trial.settings.GetString("File");
+            Debug.Log("is_mirrored = False");
+        }
         
-            Debug.Log("Name of Trial File: " + file_name);
+        Debug.Log("Name of Trial File: " + file_name);
 
-            TextAsset terrain_file = Resources.Load<TextAsset>(file_name); // feed in the file name without `.csv` appended to it
+        TextAsset terrain_file = Resources.Load<TextAsset>(file_name); // feed in the file name without `.csv` appended to it
 
-            // split up the data by line
-            string[] terrain_info = terrain_file.text.Split(new char[] { '\n' });
+        // split up the data by line
+        string[] terrain_info = terrain_file.text.Split(new char[] { '\n' });
 
-            // Loop through the length of the list and create game objects
-            // subtract 1 from length because Unity reads in an empty line at the end
-            // start at i = 1 because we're giving our csv a header
-            //Debug.Log(terrain_info.Length);
-            for (int i = 1; i < terrain_info.Length -1; i++)
-            {   
-                // Split up terrain by commas to give us each column
-                string[] col = terrain_info[i].Split(new char[] { ',' } );
+        // Loop through the length of the list and create game objects
+        // subtract 1 from length because Unity reads in an empty line at the end
+        // start at i = 1 because we're giving our csv a header
+        //Debug.Log(terrain_info.Length);
+        for (int i = 1; i < terrain_info.Length -1; i++)
+        {   
+            // Split up terrain by commas to give us each column
+            string[] col = terrain_info[i].Split(new char[] { ',' } );
+        
+            GameObject target = Instantiate(prefab_target);
 
-                // generate a game object based on the information in col
-                if (col[3] == "O")
-                {
-                    GameObject distractor = Instantiate(prefab_distractor);
-                    
-                    if(col[5] == "P1")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        distractor.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P2")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        distractor.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P3")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        distractor.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
-                        
-                    }
-
-                    Debug.Log("Obs Loc:" + distractor.transform.position.ToString("F4"));
-
-                }
-                
-
-                if (col[3] == "C10")
-                {
-                    GameObject target = Instantiate(prefab_target);
-                    target.transform.Rotate(0,float.Parse(col[4]),0);
-
-                    if(col[5] == "P1")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P2")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P3")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
-                        
-                    }
-
-                    Debug.Log("Tar Loc:" + target.transform.position.ToString("F4"));
-                }
-
-                if (col[3] == "C80")
-                {
-                    GameObject target = Instantiate(prefab_target);
-                    target.transform.Rotate(0,float.Parse(col[4]),0);
-
-                    if(col[5] == "P1")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P2")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P3")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
-                        
-                    }
-
-                    Debug.Log("Tar Loc:" + target.transform.position.ToString("F4"));
-                }
-
+            if(col[5] == "P1")
+            {
+                // add the x,z position of the appropriate camera to the position of the game object
+                target.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
             }
 
-        }
- 
-        if(is_mirrored == true)
-        {
-            Timer_Start_Box_Left.SetActive(false);
-            Timer_Start_Box_Right.SetActive(true);
-
-            Trial_End_Box_Left.SetActive(true);
-            Trial_End_Box_Right.SetActive(false);
-
-            string file_name = trial.settings.GetString("Mirror");
-        
-            Debug.Log("Name of Trial File: " + file_name);
-
-            TextAsset terrain_file = Resources.Load<TextAsset>(file_name); // feed in the file name without `.csv` appended to it
-
-            // split up the data by line
-            string[] terrain_info = terrain_file.text.Split(new char[] { '\n' });
-
-            // Loop through the length of the list and create game objects
-            // subtract 1 from length because Unity reads in an empty line at the end
-            // start at i = 1 because we're giving our csv a header
-            //Debug.Log(terrain_info.Length);
-            for (int i = 1; i < terrain_info.Length -1; i++)
-            {   
-                // Split up terrain by commas to give us each column
-                string[] col = terrain_info[i].Split(new char[] { ',' } );
-
-                // generate a game object based on the information in col
-                if (col[3] == "O")
-                {
-                    GameObject distractor = Instantiate(prefab_distractor);
-                    
-                    if(col[5] == "P1")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        distractor.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P2")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        distractor.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P3")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        distractor.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
-                        
-                    } 
-
-                    Debug.Log("Obs Loc:" + distractor.transform.position.ToString("F4"));
-
-                }
-                
-
-                if (col[3] == "C10")
-                {
-                    GameObject target = Instantiate(prefab_target);
-                    target.transform.Rotate(0,float.Parse(col[4]),0);
-
-                    if(col[5] == "P1")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P2")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P3")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
-                        
-                    }
-
-                    Debug.Log("Tar Loc:" + target.transform.position.ToString("F4"));
-                }
-
-                if (col[3] == "C80")
-                {
-                    GameObject target = Instantiate(prefab_target);
-                    target.transform.Rotate(0,float.Parse(col[4]),0);
-
-                    if(col[5] == "P1")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector1_Pos_XYZ[0],0,float.Parse(col[2])+Projector1_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P2")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
-                        
-                    }
-
-                    if(col[5] == "P3")
-                    {
-                        // add the x,z position of the appropriate camera to the position of the game object
-                        target.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
-                        
-                    }
-
-                    Debug.Log("Tar Loc:" + target.transform.position.ToString("F4"));
-                }
-
+            if(col[5] == "P2")
+            {
+                // add the x,z position of the appropriate camera to the position of the game object
+                target.transform.position = new Vector3(float.Parse(col[0])+Projector2_Pos_XYZ[0],0,float.Parse(col[2])+Projector2_Pos_XYZ[2] );
             }
 
-        }
+            if(col[5] == "P3")
+            {
+                // add the x,z position of the appropriate camera to the position of the game object
+                target.transform.position = new Vector3(float.Parse(col[0])+Projector3_Pos_XYZ[0],0,float.Parse(col[2])+Projector3_Pos_XYZ[2] );
+            }
 
+            Debug.Log("Tar Loc:" + target.transform.position.ToString("F4"));
+        }
     }
-
 }
